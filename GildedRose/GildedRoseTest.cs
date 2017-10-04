bc59148da4127e1d86c09a4c9c4cd051698e6696
jfaber <jhonatanfaber@gmail.com>
2017-10-04 22:25:51 +0100

    First version of GildedRose

---
+++ GildedRose/GildedRoseTest.cs
@@ -0,0 +1,277 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace GildedRose{
+    [TestFixture]
+    public class GildedRoseTest{
+        
+        [SetUp] 
+        public void SetUp(){
+            
+        }
+
+        [Test]
+        public void check_dexterity_Vests_name(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 10,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual("+5 Dexterity Vest", Items[0].Name);
+        }
+
+        [Test]
+        public void check_dexterity_Vests_sellIn_decreased_by_1(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 9,
+                    Quality = 19
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].SellIn);
+        }
+        
+        [Test]
+        public void check_dexterity_Vests_quality_decreased_by_1(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 9,
+                    Quality = 19
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(18, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_dexterity_Vests_quality_decreases_double_when_date_has_passed(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "+5 Dexterity Vest",
+                    SellIn = 0,
+                    Quality = 10
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(8, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_dexterity_Vests_quality_is_not_negative(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "+5 Dexterity Vest",
+                    SellIn = -5,
+                    Quality = 0
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_age_bries_quality_increases_by_1(){
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "Aged Brie",
+                    SellIn = 2,
+                    Quality = 0
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(1, Items[0].Quality);
+        }
+
+        [Test]
+        public void check_age_bries_quality_increases_by_2_when_date_has_passed(){
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "Aged Brie",
+                    SellIn = 0,
+                    Quality = 2
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_age_bries_quality_is_never_more_than_50(){
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "Aged Brie",
+                    SellIn = -24,
+                    Quality = 50
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_sulfuras_quality_never_decreases(){
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = 0,
+                    Quality = 80
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(80, Items[0].Quality);
+        }
+        
+        
+        [Test]
+        public void check_sulfuras_quality_never_decreases_when_date_has_passed(){
+            IList<Item> Items = new List<Item>{
+                new Item {Name = "Sulfuras, Hand of Ragnaros",
+                    SellIn = -1,
+                    Quality = 80
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(80, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_increases_by_1(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 15,
+                    Quality = 20
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(21, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_increases_by_2_when_10days_or_less(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 47
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(49, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_increases_by_2_when_10days_or_less_and_doesnt_overpass_50(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 10,
+                    Quality = 49
+                },
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_increases_by_3_when_5days_or_less(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 46
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(49, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_increases_by_3_when_5days_or_less_and_doesnt_overpass_50(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 5,
+                    Quality = 48
+                },
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(50, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_backstagePasses_quality_decreases_to_0_when_date_has_passed(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 0,
+                    Quality = 50
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_conjured_quality_decreases_by_2(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Conjured Mana Cake",
+                    SellIn = 3,
+                    Quality = 6
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(4, Items[0].Quality);
+        }
+        
+        [Test]
+        public void check_conjured_quality_decreases_by_4_when_date_has_passed(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Conjured Mana Cake",
+                    SellIn = -1,
+                    Quality = 6
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(2, Items[0].Quality);
+        }
+        
+        // creo que este sobra
+        [Test]
+        public void check_conjured_quality_reachs_zero(){
+            IList<Item> Items = new List<Item>{
+                new Item{
+                    Name = "Conjured Mana Cake",
+                    SellIn = -2,
+                    Quality = 2
+                }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(0, Items[0].Quality);
+        }
+        
+    }
+}
---
+++ GildedRose/Item.cs
@@ -0,0 +1,27 @@
+﻿namespace GildedRose{
+    public class Item{ // do not alter the Item class or Items property 
+        public string Name { get; set; }
+        public int SellIn { get; set; }//  number of days we have to sell the item
+        public int Quality { get; set; } //  how valuable the item is
+        // At the end of each day our system lowers both values for every item
+    }
+    
+    /**
+    GAME RULES:
+    
+    Once the sell by date has passed, Quality degrades twice as fast
+    The Quality of an item is never negative
+    “Aged Brie” actually increases in Quality the older it gets
+    The Quality of an item is never more than 50
+    “Sulfuras”, being a legendary item, never has to be sold or decreases in Quality
+    “Backstage passes”, like aged brie, increases in Quality as its SellIn value approaches;
+        Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+        Quality drops to 0 after the concert
+        
+     Your task: “Conjured” items degrade in Quality twice as fast as normal items
+     
+     Just for clarification, an item can never have its Quality increase above 50, however “Sulfuras” 
+     is a legendary item and as such its Quality is 80 and it never alters.
+
+    */
+}
